a5b59a3d5daf316acb436ec11c85178a3553ce62
Pavel Cistjakov <pavelcistjakov@gmail.com>
2022-06-30 07:59:01 +0100

    Adding a data class for questions and minor tweaks in UI

--- app/src/main/java/android/banananabandit/memequizapp/MainActivity.kt
+++ app/src/main/java/android/banananabandit/memequizapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package android.banananabandit.memequizapp
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +19,17 @@ class MainActivity : AppCompatActivity() {
             window.decorView.windowInsetsController!!.hide(
                 android.view.WindowInsets.Type.statusBars()
             )
+        }
+        val buttonStartQuiz = findViewById<Button>(R.id.buttonStart)
+        val nameEntry = findViewById<EditText>(R.id.nameEntry)
+        buttonStartQuiz.setOnClickListener {
+            if (nameEntry.text.isNotEmpty()) {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else if (nameEntry.text.isEmpty()) {
+                Toast.makeText(this, "Enter your name, dum-dum", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
---
+++ app/src/main/java/android/banananabandit/memequizapp/Question.kt
@@ -0,0 +1,12 @@
+package android.banananabandit.memequizapp
+
+data class Question(
+    val question : String,
+    val questionId : Int,
+    val image : Int,
+    val optOne : String,
+    val optTwo : String,
+    val optThree : String,
+    val optFour : String
+
+)
