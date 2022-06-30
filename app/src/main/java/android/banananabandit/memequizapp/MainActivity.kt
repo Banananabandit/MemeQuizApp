609eb6c8f3a1ce6dc4646986d3e91a3bc1c78bc1
Pavel Cistjakov <pavelcistjakov@gmail.com>
2022-06-30 06:10:39 +0100

    Adding UI layout for the welcome screen

--- app/src/main/java/android/banananabandit/memequizapp/MainActivity.kt
+++ app/src/main/java/android/banananabandit/memequizapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package android.banananabandit.memequizapp
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); // This is to disable the night mode
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.decorView.windowInsetsController!!.hide(
+                android.view.WindowInsets.Type.statusBars()
+            )
+        }
     }
 }
