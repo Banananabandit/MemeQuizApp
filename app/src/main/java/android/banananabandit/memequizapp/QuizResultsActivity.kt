39b89407c3251b155c81d90dac998a7b368b9ed1
Pavel Cistjakov <pavelcistjakov@gmail.com>
2022-07-06 08:55:06 +0100

    Adding basic animation to results text

--- app/src/main/java/android/banananabandit/memequizapp/QuizResultsActivity.kt
+++ app/src/main/java/android/banananabandit/memequizapp/QuizResultsActivity.kt
@@ -1,8 +1,15 @@
 package android.banananabandit.memequizapp
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.Color.parseColor
+import android.graphics.LinearGradient
+import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.LinearInterpolator
 import android.widget.Button
 import android.widget.TextView
 
@@ -16,13 +23,23 @@ class QuizResultsActivity : AppCompatActivity() {
         val nameTextView = findViewById<TextView>(R.id.result_name)
         val finalResult = findViewById<TextView>(R.id.final_result)
         val finishButton = findViewById<Button>(R.id.finish_button)
+        val nothingTextView = findViewById<TextView>(R.id.nothing)
 
         val name = intent.getStringExtra(Constants.USER_NAME)
         val correctAns = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
         val totalAns = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
 
+
+        val colorAnimation = ObjectAnimator.ofInt(nothingTextView, "textColor", Color.RED, Color.BLUE, Color.GREEN, Color.MAGENTA)
+        colorAnimation.duration = 10000
+        colorAnimation.interpolator = LinearInterpolator()
+        colorAnimation.repeatCount = 4
+        colorAnimation.start()
+
         nameTextView.text = "$name, your result is:"
         finalResult.text = "$correctAns/$totalAns"
+
+
 
         finishButton.setOnClickListener {
             val finalIntent = Intent(this, MainActivity::class.java)
